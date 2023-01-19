d5fab138b11dcafed73bc1027425a97b48336b0a
FreakFlame <therealfreakflame@gmail.com>
2023-01-19 14:43:37 +0100

    yeah

---
+++ BjarnesKids/BjarnesKids/Almutasim.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BjarnesKids
+{
+    internal class Almutasim
+    {
+        
+        int howLongCanAVariableNameBeOhWowLooksLikeItCanBePrettyLong = 57;
+        double trouble = 2.5;
+        string spider = "you get string from spiders";
+        bool isTrue = true;
+        
+        public void Run()
+        {
+            Console.WriteLine(MyAlmutasim(howLongCanAVariableNameBeOhWowLooksLikeItCanBePrettyLong));
+        }
+        
+        string MyAlmutasim(int howLongCanAVariableNameBeOhWowLooksLikeItCanBePrettyLong)
+        {
+            return spider;
+        }
+
+    }
+}
