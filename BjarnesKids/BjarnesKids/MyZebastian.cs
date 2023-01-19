531c35dbbc8ee96ca67b1a237532552b6d019458
TheShadow0600 <59416747+TheShadow06001@users.noreply.github.com>
2023-01-19 14:42:46 +0100

    Jag har gjort en kod som skriver ut hello world och hola mundo 23 gånger (tillsammans)

--- BjarnesKids/BjarnesKids/MyZebastian.cs
+++ BjarnesKids/BjarnesKids/MyZebastian.cs
@@ -11,7 +11,52 @@ namespace BjarnesKids
         public void Run()
         {
 
+            Console.WriteLine(ConvertInt(value1));
+           
+
+            for (int i = 0; i < value1; i++)
+            {
+                ConsoleString(dunk);
+                ConvertInt(value1);
+
+            }
+
         }
+
+        public  int value1 = 20;
+        public float kill = 10f;
+        public string dunk = "Hello World!";
+        public bool pressed = true;
+
+
+
+        string ConvertInt(int value2)
+        {
+            string text = "Hello World!";
+
+
+            if (value2 > 1)
+            {
+                Console.WriteLine(text);
+            }
+
+
+            
+
+            return text;
+            
+        }
+
+
+        void  ConsoleString(string skibop)
+        {
+
+            skibop = "Hola Mundo!";
+
+            Console.WriteLine(skibop);
+        }
+
+
 
     }
 }
