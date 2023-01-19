84af99d915801889d7581df162716a41b53c6bd2
Guldfisken34 <122522047+Guldfisken34@users.noreply.github.com>
2023-01-19 12:09:00 +0100

    carls cod

---
+++ BjarnesKids/BjarnesKids/MyCarl.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BjarnesKids
+{
+    internal class MyCarl
+    {
+        public void Run() 
+        {
+            string[] show = new string[2];
+            show[0] = " ";
+            show[1] = "Hello world";
+
+
+            int witch = 0;
+
+
+            string key = Console.ReadKey().Key.ToString();
+            if (key.ToUpper() == "W") 
+            {
+
+                witch = 1;
+            
+            
+            
+            }
+            else 
+            {
+
+                witch = 0;
+            
+            }
+
+            Console.WriteLine(show[witch]);
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+}
--- BjarnesKids/BjarnesKids/Program.cs
+++ BjarnesKids/BjarnesKids/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using BjarnesKids;
+
+
+{
+    MyCarl myCarl;
+    myCarl = new MyCarl();
+    myCarl.Run();
+}
+
