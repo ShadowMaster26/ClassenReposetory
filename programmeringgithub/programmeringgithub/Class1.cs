06cf0faa7f191b9e1cbd3b7b6737e05ac35faa94
B0eL1 <construx.super@gmail.com>
2023-01-19 14:41:34 +0100

    dha
    su2

---
+++ programmeringgithub/programmeringgithub/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programmeringgithub
+{
+    internal class Class1
+    {
+    }
+}
