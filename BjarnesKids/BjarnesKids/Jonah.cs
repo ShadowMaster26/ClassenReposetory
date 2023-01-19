1ae25e2bc10e2805bfa9e29c6f87e8d453cb4533
PiranhaPlant9915 <jonah@aplamon.com>
2023-01-19 14:41:43 +0100

    Variabel och metoduppgift

---
+++ BjarnesKids/BjarnesKids/Jonah.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BjarnesKids
+{
+    internal class Jonah
+    {
+        string myName = "Jonah";
+        int myFavoriteNumber = 9;
+        float coolNumber = 22.5F;
+        bool isEpic = false;
+
+        public void Run()
+        {
+            for (int i = 0; i < coolNumber; i++)
+            {
+                WriteString(GetPerson(myFavoriteNumber));
+
+            }
+
+        }
+
+        string GetPerson(int favoriteNumber)
+        {
+            if (favoriteNumber == 9)
+            {
+                return myName;
+            }
+            return "RAHHH!! No person found with that favorite number!";
+        }
+
+        void WriteString(string stringToWrite)
+        {
+            Console.WriteLine(stringToWrite);
+        }
+
+        
+    }
+}
--- BjarnesKids/BjarnesKids/Program.cs
+++ BjarnesKids/BjarnesKids/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using BjarnesKids;
+{
+    Jonah jonah;
+    jonah = new Jonah();
+    jonah.Run();
+}
